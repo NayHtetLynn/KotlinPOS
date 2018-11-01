8efabe6005fb5e7241fb716fb9f639f5f52e241b
Dell <nayhtetlynn20596@gmail.com>
2018-11-01 17:32:45 +0630

    Add New Ledger

--- app/src/main/java/com/project/my/kotlinpos/Fragment/AddNewLedgerFragment.kt
+++ app/src/main/java/com/project/my/kotlinpos/Fragment/AddNewLedgerFragment.kt
@@ -25,7 +25,9 @@ class AddNewLedgerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_new_ledger, container, false)
+        val view:View=inflater.inflate(R.layout.fragment_add_new_ledger, container, false)
+
+        return view
     }
 
 
